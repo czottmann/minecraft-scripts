23a5d804a5ee9c97e71eea5212303c42f1a299f4
Carlo Zottmann <carlo@municode.de>
2010-12-01 00:06:21 +0100

    tgz is overkill.

--- backup.sh
+++ backup.sh
@@ -5,8 +5,8 @@ THEDATE=`date +%Y%m%d-%H%M`
 # minecraft sits in /var/minecraft
 
 cd /var
-tar czpf minecraft-backups/minecraft.$THEDATE.game.tgz minecraft --exclude=world
-tar czpf minecraft-backups/minecraft.$THEDATE.world.tgz minecraft/world --exclude=*.png
+tar cf minecraft-backups/minecraft.$THEDATE.game.tar minecraft --exclude=world
+tar cf minecraft-backups/minecraft.$THEDATE.world.tar minecraft/world --exclude=*.png
 
 # remove old backups, anything over a day old
 find minecraft-backups -type f -mtime +1 -exec rm -f '{}' \;
